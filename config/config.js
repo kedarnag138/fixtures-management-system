323080a76bbaa2482d5c63bcf6e212d26daa2bee
Mukanahallipatna Satish Kedarnag <Kedarnag_MUKANAHALLI_from.SCIENTEC@stb.gov.sg>
2022-10-17 15:48:09 +0800

    [main] :sparkles: 1. Updates the config.js with the new environment.

--- config/config.js
+++ config/config.js
@@ -2,12 +2,13 @@
 
 module.exports = {
 	"development": {
-		"database": "fixtures_management",
+		"database": "fixtures_management_development",
 		"host": "127.0.0.1",
 		"dialect": "mysql",
 		"port": 3306,
 		"username": "root",
-		"password": "password"
+		"password": "password",
+		"socketPath": "/var/lib/mysql/mysql.sock"
 	},
 	"test": {
 		"database": "fixtures_management_test",
@@ -17,4 +18,13 @@ module.exports = {
 		"username": "root",
 		"password": "password"
 	},
+	"dev": {
+		"database": "heroku_51951fb98ebdf24",
+		"host": "eu-cdbr-west-03.cleardb.net",
+		"dialect": "mysql",
+		"port": 3306,
+		"username": "b47f9a1030846a",
+		"password": "bbb0a210",
+
+	}
 }
